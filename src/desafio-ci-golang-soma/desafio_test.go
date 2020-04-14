58e62ea4ce5529e16dd413c325ea1f2ce3e8e22a
FelipePereiraAlves <54959326+FelipePereiraAlves@users.noreply.github.com>
2020-04-14 16:32:31 -0300

    a

---
+++ src/desafio-ci-golang-soma/desafio_test.go
@@ -0,0 +1,14 @@
+package desafio_ci_golang_soma
+
+import (
+	"testing"
+)
+
+func TestSoma(t *testing.T) {
+	actualResult := soma(5, 5)
+	var expectedResult = 10
+
+	if actualResult != expectedResult {
+		t.Errorf("Soma incorreta, obtido: %d, esperado: %d.", actualResult, expectedResult)
+	}
+}
